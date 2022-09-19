8adeb83cfcadb51aa4e7790ed4a57fa2bea3dccd
Anuoluwapo Akinseye <Anuoluwapo.Akinseye@sterling.ng>
2022-09-19 18:04:43 +0100

    setup the state management module

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import example from './module-example'
+import storeModule from './store-module'
 
 Vue.use(Vuex)
 
@@ -17,7 +17,7 @@ Vue.use(Vuex)
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      storeModule
     },
 
     // enable strict mode (adds overhead!)
