edebd593178b0ca9e29f30b827c924e2fad5d42e
Anuoluwapo Akinseye <Anuoluwapo.Akinseye@sterling.ng>
2022-09-20 05:27:24 +0100

    improve responsiveness

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,75 @@
 <template>
-    <q-header>
-      <q-toolbar class="bg-white">
-        <q-toolbar-title shrink class="header-text--purple q-mx-xl">
-          Paypay.
-        </q-toolbar-title>
+    <div>
+        <q-header>
+            <q-toolbar class="bg-white">
+                <q-toolbar-title :shrink="$q.screen.gt.sm" class="header-text--purple q-mx-xl">
+                    Paypay.
+                </q-toolbar-title>
 
-        <div class="text-center">
-            <q-tabs dense active-class="header-link--purple" align="center" inline-label shrink class="text-dark--black">
-                <q-route-tab 
-                    :key="tabItem.name"
-                    :to="tabItem.link"
-                    :icon="tabItem.icon"
-                    :name="tabItem.name"
-                    :label="tabItem.label"
-                    :class="{ 'q-mr-md': index < (tabItems.length -1) }"
-                    v-for="(tabItem, index) of tabItems"
-                    no-caps
-                    class="header-link q-py-lg q-px-sm"
-                />
-            </q-tabs>
+                <div class="gt-sm">
+                    <q-tabs dense active-class="header-link--purple" align="center" inline-label shrink class="text-dark--black">
+                        <q-route-tab 
+                            :key="tabItem.name"
+                            :to="tabItem.link"
+                            :icon="tabItem.icon"
+                            :name="tabItem.name"
+                            :label="tabItem.label"
+                            :class="{ 'q-mr-md': index < (tabItems.length -1) }"
+                            v-for="(tabItem, index) of tabItems"
+                            no-caps
+                            class="header-link q-py-lg q-px-sm"
+                        />
+                    </q-tabs>
+                </div>
+
+                <div class="lt-md">
+                    <div class="q-pt-lg"></div>
+                    <q-btn
+                        flat
+                        icon="menu"
+                        aria-label="Menu"
+                        @click="leftDrawerOpen = !leftDrawerOpen"
+                        class="bg-purple"
+                    />
+                    <div class="q-pb-lg"></div>
+                </div>
+            
+            </q-toolbar>
+        </q-header>
+        <div v-if="$q.screen.lt.md">
+            <q-drawer
+                v-model="leftDrawerOpen"
+                show-if-above
+                bordered
+                content-class="bg-grey-1"
+            >
+                <q-list class="text-dark--black">
+                    <q-item-label
+                        header
+                        class="text-grey-8"
+                    >
+                        Navigation
+                    </q-item-label>
+                    <app-side-bar
+                        v-for="link in tabItems"
+                        :key="link.name"
+                        v-bind="link"
+                    />
+                </q-list>
+            </q-drawer>
         </div>
-
-      </q-toolbar>
-    </q-header>
+    </div>
 </template>
 <script>
+import AppSideBar from './SideBar'
 export default {
     name: "Header",
+    components: {
+        AppSideBar
+    },
     data() {
         return {
+            leftDrawerOpen: false,
             tabItems: [
                 {
                     icon: 'grid_view',
---
+++ src/components/SideBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    :to="link"
+    exact
+    active-class="header-link--purple"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label class="text-bold">{{ label }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+
+export default {
+  name: 'EssentialLink',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+
+    label: {
+      type: String,
+      default: ''
+    },
+
+    link: {
+      type: String,
+      default: '#'
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
